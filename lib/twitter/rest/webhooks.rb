9e82571ea2a21f0398c0b87c6d6699e8c5b00156
Liuda Morozova <liuda315@gmail.com>
2018-07-25 11:33:04 +0100

    try twitter headers for private_media

--- lib/twitter/rest/webhooks.rb
+++ lib/twitter/rest/webhooks.rb
@@ -7,10 +7,10 @@ module Twitter
   module REST
     module Webhooks
       def subscribe_to_webhooks
-        auth_header = Twitter::Headers.new(self, :post, 'https://api.twitter.com/1.1/account_activity/all/staging/subscriptions.json')
+        auth_header = Twitter::Headers.new(self, :post, 'https://api.twitter.com/1.1/account_activity/all/envlogicalware/subscriptions.json')
 
         response = Faraday.post do |req|
-          req.url('https://api.twitter.com/1.1/account_activity/all/staging/subscriptions.json')
+          req.url('https://api.twitter.com/1.1/account_activity/all/envlogicalware/subscriptions.json')
           req.headers['Authorization'] = auth_header.oauth_auth_header.to_s
         end
 
@@ -18,10 +18,10 @@ module Twitter
       end
 
       def unsubscribe_from_webhooks
-        auth_header = Twitter::Headers.new(self, :delete, 'https://api.twitter.com/1.1/account_activity/all/staging/subscriptions.json')
+        auth_header = Twitter::Headers.new(self, :delete, 'https://api.twitter.com/1.1/account_activity/all/envlogicalware/subscriptions.json')
 
         response = Faraday.delete do |req|
-          req.url('https://api.twitter.com/1.1/account_activity/all/staging/subscriptions.json')
+          req.url('https://api.twitter.com/1.1/account_activity/all/envlogicalware/subscriptions.json')
           req.headers['Authorization'] = auth_header.oauth_auth_header.to_s
         end
         response.status == 204
